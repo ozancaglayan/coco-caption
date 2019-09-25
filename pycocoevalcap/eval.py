a216e5500ccf453ab6e3c1ff9e4295dab740db8a
Ozan Caglayan <ozancag@gmail.com>
2019-09-25 12:29:50 +0100

    CocoEval: disable SPICE, stitch BPEs and hyphens, remove print()'s

--- pycocoevalcap/eval.py
+++ pycocoevalcap/eval.py
@@ -1,4 +1,6 @@
 __author__ = 'tylin'
+import re
+
 from .tokenizer.ptbtokenizer import PTBTokenizer
 from .bleu.bleu import Bleu
 from .meteor.meteor import Meteor
@@ -15,19 +17,25 @@ class COCOEvalCap:
         self.coco = coco
         self.cocoRes = cocoRes
         self.params = {'image_id': coco.getImgIds()}
+        self.cleanup_fn = lambda s: re.sub(
+            '\s*@-@\s*', '-', s.replace("@@ ", "").replace("@@", ""))
 
-    def evaluate(self):
+    def postprocess(self, caps):
+        for cap in caps:
+            cap['caption'] = self.cleanup_fn(cap['caption'])
+        return caps
+
+    def evaluate(self, verbose=True):
         imgIds = self.params['image_id']
         gts = {}
         res = {}
         for imgId in imgIds:
-            gts[imgId] = self.coco.imgToAnns[imgId]
-            res[imgId] = self.cocoRes.imgToAnns[imgId]
+            gts[imgId] = self.postprocess(self.coco.imgToAnns[imgId])
+            res[imgId] = self.postprocess(self.cocoRes.imgToAnns[imgId])
 
         # =================================================
         # Set up scorers
         # =================================================
-        print('tokenization...')
         tokenizer = PTBTokenizer()
         gts = tokenizer.tokenize(gts)
         res = tokenizer.tokenize(res)
@@ -35,31 +43,36 @@ class COCOEvalCap:
         # =================================================
         # Set up scorers
         # =================================================
-        print('setting up scorers...')
         scorers = [
             (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
             (Meteor(), "METEOR"),
             (Rouge(), "ROUGE_L"),
             (Cider(), "CIDEr"),
-            (Spice(), "SPICE"),
+            #(Spice(), "SPICE"),
         ]
 
         # =================================================
         # Compute scores
         # =================================================
+        score_dict = {}
         for scorer, method in scorers:
-            print('computing {} score...'.format(scorer.method()))
             score, scores = scorer.compute_score(gts, res)
             if type(method) == list:
                 for sc, scs, m in zip(score, scores, method):
                     self.setEval(sc, m)
                     self.setImgToEvalImgs(scs, list(gts.keys()), m)
-                    print("{}: {:.3f}".format(m, sc))
+                    if verbose:
+                        print("{}: {:.3f}".format(m, sc))
+                    score_dict[m] = sc
             else:
                 self.setEval(score, method)
                 self.setImgToEvalImgs(scores, list(gts.keys()), method)
-                print("{}: {:.3f}".format(method, score))
+                if verbose:
+                    print("{}: {:.3f}".format(method, score))
+                score_dict[method] = score
         self.setEvalImgs()
+
+        return score_dict
 
     def setEval(self, score, method):
         self.eval[method] = score
